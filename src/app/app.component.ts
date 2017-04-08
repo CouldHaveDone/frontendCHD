56d4237a8ee9276a1eea386d52d2d86a3e3f3486
Beerend Gerats <b.gerats@innovadis.com>
2017-04-08 13:51:57 +0200

    dignen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ export class AppComponent {
   title = 'Could Have Done';
 
   ngOnInit() {
-    alert("Je kad nu al de eerste 10 seconden van Zanger Rinus - Wil met Sharon zweven in een luchtballen kunnen horen!!!");
+    //alert("Je kad nu al de eerste 10 seconden van Zanger Rinus - Wil met Sharon zweven in een luchtballen kunnen horen!!!");
   }
 }
