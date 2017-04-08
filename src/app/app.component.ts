a860aad75165d5ae581b857dafef531172c4fd73
Beerend Gerats <b.gerats@innovadis.com>
2017-04-08 13:22:08 +0200

    bootstrap + titel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Could Have Done';
+
+  ngOnInit() {
+    alert("Je kad nu al de eerste 10 seconden van Zanger Rinus - Wil met Sharon zweven in een luchtballen kunnen horen!!!");
+  }
 }
